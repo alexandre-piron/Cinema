c60117a3b984b0712428af5435bc77119cab5100
Idayat03 <idayat.noufou@ynov.com>
2023-01-08 16:20:27 +0100

    happy birthday mail + quelques remodif de la bdd

---
+++ resources/views/mails/happybirthday.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>joyeux anniversaire !!!</title>
+</head>
+<body>
+    <p>Bonjour {{$firstname}}, toute notre équipe vous souhaite un joyeux anniversaire.</p>
+
+</body>
+</html>
